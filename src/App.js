6dd275baebf6855c4a047298734301e7cb2d009b
Hannah Coley <hannah.miller.coley@gmail.com>
2018-10-07 14:17:31 -0400

    Map with markers working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,120 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
+import SideNav from './components/SideNav.js';
+
+
 
 class App extends Component {
+
+  state = {
+    restaurants: []
+  }
+
+  componentDidMount() {
+    this.getRestaurants()
+  }
+
+  renderMap = () => {
+    //passes Google Maps my API key
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAgXzDKjlWqOt-iPr3sJvcZuCIXbuQ4LlM&callback=initMap')
+    window.initMap = this.initMap
+    console.log('Map Rendered!')
+  }
+
+  initMap = () => {
+    //initialize the map
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 38.8706391, lng: -104.770058},
+      zoom: 10.83
+    })
+
+    let infoWindow = new window.google.maps.InfoWindow({
+      maxWidth: 200
+    })
+
+    this.state.restaurants.forEach(myRestaurant => {
+      //restaurant content info string
+      let contentInfo =
+        '<div className="content">' +
+          '<div className="siteNotice">' +
+          '</div>' +
+          '<h1 id="firstHeading className="firstHeading">' + myRestaurant.venue.name + '</h1>' +
+          '<div className="bodyContent">' +
+            '<p><strong>Location: </strong><br>' +
+            myRestaurant.venue.location.formattedAddress[0] + '<br>' +
+            myRestaurant.venue.location.formattedAddress[1] + '<br>' +
+            myRestaurant.venue.location.formattedAddress[2] + '</p>' +
+            /*myRestaurant.venue.delivery ?
+              '<p><strong>Delivery:  </strong><br>' +
+              myRestaurant.venue.delivery +
+              '</p>' : '' +*/
+          '</div>' +
+        '</div>';
+
+      //Create a marker for each restaurant location
+      const marker = new window.google.maps.Marker({
+        position: {lat: myRestaurant.venue.location.lat, lng: myRestaurant.venue.location.lng},
+        map: map,
+        title: myRestaurant.venue.name,
+      })
+
+      //Creates an event listener for each Marker
+      marker.addListener('click', ()=> {
+        infoWindow.setContent(contentInfo);
+        infoWindow.open(map,marker)
+      })
+    })
+  }
+
+  getRestaurants = () => {
+    //Creates Endpoint and Query Parameters
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: '0SV2TNKYX2QKFWQFYNPJN2D3MVL1DQ4FGWB2HUHODSGFZT1O',
+      client_secret: 'N5ZNTGGYEVKPMT2YPUQYQCZJIFOGGGJKGSLGANFJVMJ2EZKS',
+      query: 'sushi',
+      near: 'Colorado Springs, CO',
+      v: '20181006'
+    }
+
+    //Uses Axios to query foursquare api
+    //Axios processes http get requests
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          restaurants: response.data.response.groups[0].items
+        //calls render map
+        }, this.renderMap())
+        console.log(response)
+      })
+      .catch(error => {
+        console.log('Error: '+ error)
+      })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='wholePage'>
+        <SideNav />
+
+        <div id='map'>
+        </div>
       </div>
+
     );
   }
+}
+
+function loadScript (url) {
+  const index = window.document.getElementsByTagName('script')[0]
+  const script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
---
+++ src/components/SideNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './SideNav.css';
+
+class SideNav extends Component {
+
+  render() {
+    return(
+      <div className="sideNav">
+      I love Chris Coley
+      </div>
+    )
+  }
+}
+
+export default SideNav;
